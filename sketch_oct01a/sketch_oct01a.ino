fcd67ed7de52ff70d013c2594ba5561e6bb6b163
AceTwin <cara.cornel@byu.edu>
2018-10-01 12:48:11 -0600

    Testing 1 2 3

--- sketch_oct01a/sketch_oct01a.ino
+++ sketch_oct01a/sketch_oct01a.ino
@@ -1,9 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  void loop() {
+    digitalWrite(LEDBUILTIN, HIGH); //turn LED on
+    delay (1000); //wait for 1 sec
+    digitalWrite(LEDBUILTIN, LOW); //turn LED off
+    delay(1000); //wait for 1 sec  
+  }
 
 }
