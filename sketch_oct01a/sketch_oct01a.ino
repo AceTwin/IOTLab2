fb4536d6d2be694695297e830ec8ab69b7b7de2c
AceTwin <cara.cornel@byu.edu>
2018-10-01 13:00:00 -0600

    Testing led blinking example

--- sketch_oct01a/sketch_oct01a.ino
+++ sketch_oct01a/sketch_oct01a.ino
@@ -1,15 +1,13 @@
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
-
+//run the example.
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-  void loop() {
-    digitalWrite(LEDBUILTIN, HIGH); //turn LED on
-    delay (1000); //wait for 1 sec
-    digitalWrite(LEDBUILTIN, LOW); //turn LED off
-    delay(1000); //wait for 1 sec  
-  }
-
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
